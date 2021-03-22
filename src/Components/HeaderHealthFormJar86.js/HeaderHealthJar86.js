1b9239aad2b5d324a205bb9c1dcf71044e1d811b
Jhonatan Araneda <jharanedac@gmail.com>
2021-03-21 22:50:23 -0700

    Added Components folder, HeaderForm components

---
+++ src/Components/HeaderHealthFormJar86.js/HeaderHealthJar86.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import HealthForm from '../HeaderHealthJar86/HeaderHealthJar86'
+
+const HeaderHealthJar86 = () => {
+    return (
+        
+            <form className="checkForm">
+                <h1>Health and Wellness Report</h1>
+                <table className='table'>
+                    <thead>
+                        <tr><td>Fecha de hoy</td></tr>
+                        <tr><th><input type='text' placeholder='Search here...'></input></th></tr>
+                        <tr>
+                            <th className='col-1'>Name</th>
+                            <th className='col-1'>Birth Date</th>
+                            <th className='col-1'>Morning</th>
+                            <th className='col-1'>Evening</th>
+                            <th className='col-1'>Night</th>
+                            <th className='col-1'>View Profile</th>
+                        </tr>
+                    </thead>
+                        <HealthForm />
+                </table>
+            </form>
+        
+    )
+}
+
+export default HeaderHealthJar86
