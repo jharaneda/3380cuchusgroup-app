59b795f5d34ebdce410e73ca43b036a11e37a8fc
Jhonatan Araneda <jharanedac@gmail.com>
2021-03-21 23:10:42 -0700

    Update app.js added router and route to each feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import HeaderHealthJar86 from "./Components/HeaderHealthFormJar86.js/HeaderHealthJar86";
+import IncidentFormJar86 from "./Components/IncidentFormJar86/IncidentFormJar86";
+import NavbarJar86 from "./Components/NavbarJar86/NavbarJar86";
+import ProfileFormJar86 from "./Components/ProfileFormJar86/ProfileFormJar86";
+import TodoListFormJar86 from "./Components/TodoListFormJar86/TodoListFormJar86";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <NavbarJar86>
+          <Switch>
+            <Route path='/' exact component={HeaderHealthJar86} />
+            <Route path='/incident' exact component={IncidentFormJar86} />
+            <Route path='/profile' exact component={ProfileFormJar86} />
+            <Route path='/todolist' exact component={TodoListFormJar86} />
+            {/* <Route path='/' exact component={HeaderHealthJar86} /> */}
+          </Switch>
+        </NavbarJar86>
+      </Router>
     </div>
   );
 }
